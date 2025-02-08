3cc5cf8ae6c057b78403230a51ab73b0819226fe
vietanh3101 <anhvvph42683@fpt.edu.vn>
2025-02-08 12:19:14 +0700

    luu du lieu vao database

--- app/Controllers/Admin/UserController.php
+++ app/Controllers/Admin/UserController.php
@@ -25,6 +25,63 @@ class UserController{
         include 'app/Views/Admin/add-user.php';
     }
 
+    public function checkValidate(){
+        $name = $_POST['name'];
+        $email = $_POST['email']; 
+        $address = $_POST['address'];
+        $phone = $_POST['phone'];
+        $role = $_POST['role'];
+
+        if($name != "" && $email != "" && $address != "" && $phone != "" && $role != ""){
+            return true;
+        }else{
+            $_SESSION['error'] = "Bạn nhập thiếu thông tin";
+            return false;
+        }
+    }
+
+    public function addPostUser(){
+        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+            if(!$this->checkValidate()){
+                header("Location: ?role=admin&act=add-user");
+                exit;
+            }
+            // Thêm ảnh
+            $uploadDir = 'assets/Admin/upload/';
+            $allowedTypes = ['image/jpeg', 'image/png', 'image/gif'];
+            $destPath = "";
+            if(!empty($_FILES['image']['name'])){
+                $fileTmpPath = $_FILES['image']['tmp_name'];
+                $fileType = mime_content_type($fileTmpPath);
+                $fileName = basename($_FILES['image']['name']);
+                $fileExtension = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
+
+                $newFileName = uniqid() . '.' . $fileExtension;
+
+                if(in_array($fileType, $allowedTypes)){
+                    $destPath = $uploadDir . $newFileName;
+                    if(!move_uploaded_file($fileTmpPath, $destPath)){
+                        $destPath = "";
+                    }
+                }
+
+            }
+
+            $userModel = new UserModel();
+            $message = $userModel->addUserToDB($destPath);
+
+            if($message){
+                $_SESSION['message'] = 'Thêm mới thành công';
+                header("Location: ?role=admin&act=all-user");
+                exit;
+            }else{
+                $_SESSION['message'] = 'Thêm mới không thành công';
+                header("Location: ?role=admin&act=add-user");
+                exit;
+            }
+        }
+    }
+
     public function deleteUser(){
         if(!isset($_GET['id'])){
             $_SESSION['message'] = 'Vui lòng chọn user cần xóa';
