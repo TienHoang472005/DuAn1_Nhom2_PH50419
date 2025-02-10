90a3d54853a9146b697c929ce6f634dcb266abca
TienHoang472005 <kurosama0407@gmail.>
2025-02-10 20:41:21 +0700

    hiển thị danh sách đơn hàng

---
+++ app/Controllers/Admin/OrderController.php
@@ -0,0 +1,9 @@
+<?php
+
+class OrderController{
+    public function showOrder(){
+        $orderModel = new OrderModel();
+        $orders = $orderModel->getAllOrder();
+        include 'app/Views/Admin/show-order.php';
+    }
+}
